919faff80bc1caf1a9b7dc5ebf850f3017fe2c92
Serhiy Barhamon <sergey@bargamon.ru>
2023-01-26 21:51:38 +0100

    tests for I

--- src/i.rs
+++ src/i.rs
@@ -1,3 +1,4 @@
+/// Representation of an Italic text
 #[derive(Debug)]
 pub struct I {
     text: String,
@@ -6,5 +7,28 @@ pub struct I {
 impl I {
     pub fn new<S: Into<String>>(text: S) -> Self {
         I { text: text.into() }
+    }
+}
+
+impl From<I> for String {
+    fn from(value: I) -> Self {
+        value.text
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::I;
+
+    #[test]
+    fn happy_path() {
+        let i = I::new("italic");
+        assert_eq!(i.text, "italic".to_string());
+    }
+
+    #[test]
+    fn to_string() {
+        let i: String = I::new("italic").into();
+        assert_eq!(i, "italic".to_string());
     }
 }
