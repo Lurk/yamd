781ae521c913bcb1db2a270ab4b1f03893185641
Serhiy Barhamon <sergey@bargamon.ru>
2023-01-26 11:49:33 +0100

    add tests for A

--- src/a.rs
+++ src/a.rs
@@ -1,3 +1,4 @@
+/// Representation of an anchor
 #[derive(Debug)]
 pub struct A {
     text: Option<String>,
@@ -10,5 +11,39 @@ impl A {
             text,
             url: url.into(),
         }
+    }
+}
+
+impl Into<String> for A {
+    fn into(self) -> String {
+        let text = match self.text {
+            Some(text) => text,
+            None => self.url.clone(),
+        };
+        format!("[{}]({})", text, self.url)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::A;
+
+    #[test]
+    fn happy_path() {
+        let a = A::new("https://test.io", Some("nice link".to_string()));
+        assert_eq!(a.text, Some("nice link".to_string()));
+        assert_eq!(a.url, "https://test.io");
+    }
+
+    #[test]
+    fn to_string_with_text() {
+        let a: String = A::new("https://test.io", Some("nice link".to_string())).into();
+        assert_eq!(a, "[nice link](https://test.io)".to_string());
+    }
+
+    #[test]
+    fn to_string_without_text() {
+        let a: String = A::new("https://test.io", None).into();
+        assert_eq!(a, "[https://test.io](https://test.io)".to_string());
     }
 }
