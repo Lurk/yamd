37076fd7ad93a06924800cd3bf72b51267691d87
Serhiy Barhamon <sergey@bargamon.ru>
2023-01-26 22:16:22 +0100

    add tests for B

--- src/b.rs
+++ src/b.rs
@@ -30,6 +30,26 @@ pub struct B {
     data: Vec<BContent>,
 }
 
+impl From<B> for String {
+    fn from(value: B) -> Self {
+        format!(
+            "**{}**",
+            value
+                .data
+                .into_iter()
+                .map(|element| {
+                    match element {
+                        BContent::Text(v) => v.into(),
+                        BContent::I(v) => v.into(),
+                        BContent::S(v) => v.into(),
+                    }
+                })
+                .collect::<Vec<String>>()
+                .concat()
+        )
+    }
+}
+
 impl B {
     pub fn new() -> Self {
         B { data: vec![] }
@@ -38,5 +58,26 @@ impl B {
     fn push<BC: Into<BContent>>(mut self, element: BC) -> Self {
         self.data.push(element.into());
         self
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{b::B, i::I, s::S, text::Text};
+
+    #[test]
+    fn only_text() {
+        let b: String = B::new().push(Text::new("B as bold")).into();
+        assert_eq!(b, "**B as bold**".to_string());
+    }
+
+    #[test]
+    fn multilpe_entries() {
+        let b: String = B::new()
+            .push(Text::new("B as bold "))
+            .push(I::new("Italic"))
+            .push(S::new("Strikethrough"))
+            .into();
+        assert_eq!(b, "**B as bold *Italic*~~Strikethrough~~**".to_string());
     }
 }
