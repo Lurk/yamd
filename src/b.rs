29e700f830d7b409592fb5d3cdea8255ad254570
Serhiy Barhamon <sergey@bargamon.ru>
2023-01-27 23:22:44 +0100

    fix compiler error

--- src/b.rs
+++ src/b.rs
@@ -96,7 +96,7 @@ mod tests {
         let b: String = B::from_vec(vec![
             Text::new("B as bold ").into(),
             I::new("Italic").into(),
-            S::new("Strikethrough"),
+            S::new("Strikethrough").into(),
         ])
         .into();
         assert_eq!(b, "**B as bold *Italic*~~Strikethrough~~**".to_string());
