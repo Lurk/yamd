60abddac617f8c6fd0ad902c02b1b399d6ffeed2
Serhiy Barhamon <sergey@bargamon.ru>
2023-02-02 13:07:11 +0100

    implement parser for inline code

--- src/inline_code.rs
+++ src/inline_code.rs
@@ -1,6 +1,9 @@
-use crate::p::ParagraphTags;
+use crate::{
+    p::ParagraphTags,
+    parser::{Parser, ParserPart},
+};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct InlineCode {
     text: String,
 }
@@ -23,13 +26,47 @@ impl From<InlineCode> for ParagraphTags {
     }
 }
 
+impl Parser for InlineCode {
+    fn parse(input: &str, start_position: usize) -> Option<(Self, usize)> {
+        let mut chars = Self::get_iterator(input, start_position);
+        if let Some(end_position) = chars.parse_part(vec!['`'], vec!['`']) {
+            return Some((
+                InlineCode::new(
+                    input[start_position + 1..end_position]
+                        .to_string()
+                        .replace('\n', ""),
+                ),
+                end_position,
+            ));
+        }
+        None
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::parser::Parser;
+
     use super::InlineCode;
 
     #[test]
     fn to_string() {
         let inline_code: String = InlineCode::new("const bar = 'baz'").into();
         assert_eq!(inline_code, "`const bar = 'baz'`".to_string())
+    }
+
+    #[test]
+    fn from_string() {
+        assert_eq!(InlineCode::parse("`1`", 0), Some((InlineCode::new('1'), 2)));
+        assert_eq!(
+            InlineCode::parse("not`1`", 3),
+            Some((InlineCode::new('1'), 5))
+        );
+        assert_eq!(
+            InlineCode::parse("`const \nfoo='bar'`", 0),
+            Some((InlineCode::new("const foo='bar'"), 17))
+        );
+        assert_eq!(InlineCode::parse("not`a", 3), None);
+        assert_eq!(InlineCode::parse("`const \n\nfoo='bar'`", 0), None);
     }
 }
