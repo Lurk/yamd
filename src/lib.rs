d32c929fb7e106eefcc38b27519240544c017079
Lurk <bargamon@gmail.com>
2024-09-18 22:13:30 +0200

    lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,215 +1,78 @@
-//! # Yamd - yet another markdown document flavour
+//! YAMD - Yet Another Markdown Document (flavour)
 //!
-//! Yamd is a markdown document flavour that allows to create rich documents with images, code, and more.
+//! Simplified version of [CommonMark](https://spec.commonmark.org/).
 //!
-//! ## Syntax
+//! For formatting check [YAMD](crate::nodes::Yamd) struct documentation.
 //!
-//! Each yamd document starts with metadata section  which is YAML document surrounded by "---". Metadata has next
-//! fields: title, date, image, preview, is_draft, and tags.
+//! # Reasoning
 //!
-//! Timestamp format: "%Y-%m-%dT%H:%M:%S%z" ([specifiers description](https://docs.rs/chrono/latest/chrono/format/strftime/index.html))
+//! Simplified set of rules allows to have simpler, more efficient, parser and renderer.
+//! [YAMD](crate::nodes::Yamd) does not provide render functionality, instead it is a [serde]
+//! serializable structure that allows you to write any renderer for that structure. All HTML
+//! equivalents in this doc are provided as an example to what it can be rendered.
 //!
-//! Tags are array of strings.
+//! # Difference from CommonMark
 //!
-//! is_draft is a boolean value.
+//! While YAMD tries to utilize as much CommonMark syntax as possible, there are differences.
 //!
-//! Example:
-//! ```text
-//! ---
-//! title: Yamd - yet another markdown document flavour
-//! date: 2023-01-01 00:00:00 +0000
-//! image: /image.png
-//! preview: Here you can find out more about yamd
-//! is_draft: true
-//! tags:
-//! - markdown
-//! - rust
-//! ---
+//! ## Escaping
 //!
-//! ```
-//!
-//! ## Elements:
-//!
-//! ### List
-//!
-//! Can contain nested lists. Each nesting level equals to number of spaces before list item.
-//!
-//! #### Unordered list
-//!
-//! Starts with "- " and ends with a new line
-//!
-//! #### Ordered list
-//!
-//! Starts with "+ " and ends with a new line
-//!
-//! Example:
-//! ```text
-//! - item 1
-//! - item 2
-//!  + ordered nested list
-//!   - can have nested unordered list inside ordered list
-//!  + one more ordered item
-//! - item 3
-//! ```
-//!
-//! ### Code
-//!
-//! Element that starts with "\`\`\`lang\n", ends with "\n```" and has code in between.
-//!
-//! Example:
-//! ```text
-//! \```rust
-//! let x = 1;
-//! \```
-//! ```
-//! ^ did not figured out how to escape \`\`\` in rustdoc
-//!
-//! ### Image
-//!
-//! Element that starts with "!" has image alt text in [] and followed by image url in ()
-//!
-//! Example:
-//! ```text
-//! ![alt text](url)
-//! ```
-//!
-//! ### ImageGallery
-//!
-//! Element that starts with "!!!\n", ends with "\n!!!", and has image elements in between
-//!
-//! Example:
-//! ```text
-//! !!!
-//! ![alt text](url)
-//! ![alt text](url)
-//! !!!
-//! ```
-//!
-//! ### Highlight
-//!
-//! Element that starts with ">>>\n", followed by optional title that starts with ">> " and ends with a new line,
-//! followed by optional icon specifier that starts with "> " and ends with a new line, followed by body that can
-//! contain any number of paragraph elements
-//!
-//! Example:
-//! ```text
-//! >>>
-//! >> Title
-//! > icon
-//! body
-//!
-//! can be multiple paragraphs long
-//! >>>
-//! ```
-//! no header and no icon:
-//! ```text
-//! >>>
-//! body
-//! >>>
-//! ```
-//!
-//! ### Divider
-//!
-//! Element that consist of five "-" characters in a row and ends with a new line or EOF.
-//!
-//! Example: ```-----```
-//!
-//! ### Embed
-//!
-//! Element that starts with "{{" followed by embed type, followed by "|" followed by embed url, followed by "}}"
-//! and ends with a new line or EOF.
-//!
-//! Example: ```{{youtube|https://www.youtube.com/embed/wsfdjlkjsdf}}```
-//!
-//!
-//! ### Paragraph
-//!
-//! Element that starts with any character that is not a special character and ends with a new line or EOF.
-//! Can contain text, bold text, italic text, strikethrough text, anchors, and inline code.
-//!
-//! #### Anchor
-//!
-//! element that starts with "[" followed by text, followed by "]" followed by "(" followed by url, followed by ")"
-//!
-//! example: ```[Yamd repo](https://github.com/Lurk/yamd)```
-//!
-//! #### Inline code
-//!
-//! element that starts with "\`" followed by text and ends with "\`"
-//!
-//! example: ``` `inline code` ```
-//!
-//! #### Italic text    
-//!
-//! element that starts with "\_" followed by text and ends with "\_"
-//!
-//! example: ``` _italic text_ ```
-//!
-//! #### Strikethrough text
-//!
-//! element that starts with "\~\~" followed by text and ends with "\~\~"
-//!
-//! example: ``` ~~strikethrough text~~ ```
-//!
-//! #### Bold text
-//!
-//! element that starts with "\*\*" followed by text and ends with "\*\*"
-//!
-//! example: ``` **bold text** ```
-//!
-//! Bold text can also contain italic text and strikethrough text
-//!
-//! example: ``` **bold _italic_ text** ``` or ``` **bold ~~strikethrough~~ text** ```
-//!
-//! Altogether: ``` text **bold _italic_ text** ~~strikethrough~~ text `inline code` [Yamd repo](url) ``` will be parsed into Paragraph
-//!
-//! ### Collapsible
-//!
-//! Collapsible element can contain all from the above
+//! Escaping done on a [lexer] level. Every symbol following the `\` symbol will be treated as a
+//! [literal](crate::lexer::TokenKind::Literal).
 //!
 //! Example:
 //!
-//! ```text
-//! {% Title
-//! some random text
-//! %}
-//! ```
+//! | YAMD      | HTML equivalent |
+//! |-----------|-----------------|
+//! | `\**foo**`|`<p>**foo**</p>` |
 //!
-//! Nested collapsible elements example:
+//! ## Precedence
 //!
-//! ```text
-//! {% Title
-//! some random text
+//! [CommonMark](https://spec.commonmark.org/0.31.2/#precedence) defines container blocks and leaf
+//! blocks. And that container block indicator has higher precedence. YAMD does not discriminate by
+//! block type, every node (block) is the same. In practice, there are no additional rules to encode
+//! and remember.
 //!
-//! {% Nested title
-//! some random text
-//! %}
-//! %}
-//! ```
+//! Example:
 //!
-//! ### Heading
+//! | YAMD                  | HTML equivalent                               |
+//! |-----------------------|-----------------------------------------------|
+//! | ``- `one\n- two` ``   | `<ol><li><code>one\n- two</code></li></ol>`   |
 //!
-//! Element that starts with one to seven "#" characters followed by space, followed by text and/or link, and ends
-//! with a new line or EOF
 //!
-//! Example: ```# header``` or ```###### header``` or ```# [header](url)``` or ```# header [link](url)```
+//! If you want to have two [ListItem](crate::nodes::ListItem)'s use escaping:
 //!
-
-use nodes::yamd::Yamd;
-use toolkit::parser::Parse;
-
+//! | YAMD                      | HTML equivalent                           |
+//! |---------------------------|-------------------------------------------|
+//! | ``- \`one\n- two\` ``     | ``<ol><li>`one</li><li>two`</li><ol>``    |
+//!
+//! The reasoning is that those kind issues can be caught with tooling like linters/lsp. That tooling
+//! does not exist yet.
+//!
+//! ## Nodes
+//!
+//! List of supported [nodes](crate::nodes) and their formatting. The best starting point is
+//! [YAMD](crate::nodes::Yamd).
+//!
+//!
+pub mod lexer;
 pub mod nodes;
-mod toolkit;
+mod parser;
+
+#[doc(inline)]
+pub use nodes::Yamd;
+use parser::{yamd, Parser};
 
 /// Deserialize a string into a Yamd struct
 /// # Example
 /// ```
 /// use yamd::deserialize;
 /// let input = "# header";
-/// let yamd = deserialize(input).unwrap();
+/// let yamd = deserialize(input);
 /// ```
-pub fn deserialize(input: &str) -> Option<Yamd> {
-    Yamd::parse(input, 0).map(|(yamd, _)| yamd)
+pub fn deserialize(str: &str) -> Yamd {
+    let mut p = Parser::new(str);
+    yamd(&mut p, |_| false)
 }
 
 /// Serialize a Yamd struct into a string
@@ -217,7 +80,7 @@ pub fn deserialize(input: &str) -> Option<Yamd> {
 /// ```
 /// use yamd::{deserialize, serialize};
 /// let input = "# header";
-/// let yamd = deserialize(input).unwrap();
+/// let yamd = deserialize(input);
 /// let output = serialize(&yamd);
 /// ```
 pub fn serialize(input: &Yamd) -> String {
@@ -226,18 +89,22 @@ pub fn serialize(input: &Yamd) -> String {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use crate::nodes::{anchor::Anchor, heading::Heading, paragraph::Paragraph, text::Text};
     use pretty_assertions::assert_eq;
+
+    use crate::{
+        deserialize,
+        nodes::{Anchor, Heading, Paragraph, Yamd},
+        serialize,
+    };
 
     #[test]
     fn test_deserialize() {
         let input = "# header";
         let expected = Yamd::new(
             None,
-            vec![Heading::new(1, vec![Text::new("header").into()]).into()],
+            vec![Heading::new(1, vec![String::from("header").into()]).into()],
         );
-        let actual = deserialize(input).unwrap();
+        let actual = deserialize(input);
         assert_eq!(expected, actual);
     }
 
@@ -245,7 +112,7 @@ mod tests {
     fn test_serialize() {
         let input = Yamd::new(
             None,
-            vec![Heading::new(1, vec![Text::new("header").into()]).into()],
+            vec![Heading::new(1, vec![String::from("header").into()]).into()],
         );
         let expected = "# header";
         let actual = serialize(&input);
@@ -258,11 +125,11 @@ mod tests {
         let expected = Yamd::new(
             None,
             vec![
-                Heading::new(2, vec![Text::new("🤔").into()]).into(),
+                Heading::new(2, vec![String::from("🤔").into()]).into(),
                 Paragraph::new(vec![Anchor::new("link 😉", "url").into()]).into(),
             ],
         );
-        let actual = deserialize(input).unwrap();
+        let actual = deserialize(input);
         assert_eq!(expected, actual);
     }
 }
