39c5eb9fabe631888fa40fe877db1f4f1b9dff9e
Lurk <bargamon@gmail.com>
2024-09-20 21:40:40 +0200

    code doc

--- src/nodes/code.rs
+++ src/nodes/code.rs
@@ -2,6 +2,31 @@ use std::fmt::Display;
 
 use serde::Serialize;
 
+/// # Code
+///
+/// Starts with [Backtick](type@crate::lexer::TokenKind::Backtick) of length < 3.
+///
+/// [Lang](Code::lang) is every token except [Terminator](type@crate::lexer::TokenKind::Terminator)
+/// between [Backtick](type@crate::lexer::TokenKind::Backtick) of length < 3 and
+/// [EOL](type@crate::lexer::TokenKind::Eol).
+///
+/// [Code](Code::code) is every token until [Backtick](type@crate::lexer::TokenKind::Backtick) of
+/// length < 3.
+///
+/// Example:
+///
+/// ~~~text
+/// ```rust
+/// let a = 42;
+/// ```
+/// ~~~
+///
+/// HTML equivalent:
+///
+/// ```html
+/// <pre><code class="rust">let a = 42;</code></pre>
+/// ```
+
 #[derive(Debug, PartialEq, Serialize, Clone, Eq)]
 pub struct Code {
     pub lang: String,
