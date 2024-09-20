8639bbf177235bd2d7fcda327ab6b13d311150f5
Lurk <bargamon@gmail.com>
2024-09-20 21:12:10 +0200

    add image doc

--- src/nodes/image.rs
+++ src/nodes/image.rs
@@ -3,6 +3,36 @@ use std::fmt::Display;
 use serde::Serialize;
 
 #[derive(Debug, PartialEq, Serialize, Clone)]
+/// # Image
+///
+/// Starts with [Bang](type@crate::lexer::TokenKind::Bang) of length 1, and has two required parts.
+///
+/// [Alt](Image::alt) can contain any character and is surrounded by square brackets
+/// [LeftSquareBracket](type@crate::lexer::TokenKind::LeftSquareBracket) and
+/// [RightSquareBracket](type@crate::lexer::TokenKind::RightSquareBracket) respectively.
+///
+/// [Src](Image::src) can contain any character surrounded by parenthesis
+/// [LeftParenthesis](type@crate::lexer::TokenKind::LeftParenthesis) and
+/// [RightParenthesis](type@crate::lexer::TokenKind::RightParenthesis). Must support any number of
+/// nested parenthesis.
+///
+/// Examples:
+///
+/// |                yamd                   | html equivalent                                   |
+/// |---------------------------------------|---------------------------------------------------|
+/// | `![alt](src)`                         | `<img src="src" alt="alt" />`                     |
+/// | `![alt [nested squares\]](src)`       | `<img src="src" alt="alt [nested squares]" />`    |
+/// | `![alt](src(with nested)paren)`       | `<img src="src(with nested)paren" alt="alt" />`   |
+/// | `![alt](src(with(unclosed)nested`     | `<img src="url(with(unclosed" alt="alt" />`       |
+///
+/// Examples of things that are not valid Image:
+///
+/// |                yamd                   | html equivalent                               |
+/// |---------------------------------------|-----------------------------------------------|
+/// | `![alt]`                              | `<p>![alt]</p>`                               |
+/// | `![alt](src with unclosed paren`      | `<p>![alt](src with unclosed paren</p>`       |
+///
+
 pub struct Image {
     pub alt: String,
     pub src: String,
