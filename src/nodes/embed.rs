eec3305fd8760442f4c307759e87b82341f85fce
Lurk <bargamon@gmail.com>
2024-09-20 22:11:11 +0200

    embed doc

--- src/nodes/embed.rs
+++ src/nodes/embed.rs
@@ -2,10 +2,31 @@ use std::fmt::Display;
 
 use serde::Serialize;
 
+/// # Embed
+///
+/// Starts with [LeftCurlyBrace](type@crate::lexer::TokenKind::LeftCurlyBrace) of length 2.
+///
+/// [Kind](Embed::kind) every token except [Terminator](type@crate::lexer::TokenKind::Terminator)
+/// until [Pipe](type@crate::lexer::TokenKind::Pipe).
+///
+/// [Args](Embed::args) every token except [Terminator](type@crate::lexer::TokenKind::Terminator)
+/// until [LeftCurlyBrace](type@crate::lexer::TokenKind::LeftCurlyBrace) of length 2.
+///
+/// Examples:
+///
+/// ```text
+/// {{youtube|dQw4w9WgXcQ}}
+/// ```
+///
+/// HTML equivalent:
+///
+/// ```html
+/// <iframe class="youtube" src="https://www.youtube.com/embed/dQw4w9WgXcQ"></iframe>
+/// ```
 #[derive(Debug, PartialEq, Serialize, Clone, Eq)]
 pub struct Embed {
-    pub args: String,
     pub kind: String,
+    pub args: String,
 }
 
 impl Embed {
