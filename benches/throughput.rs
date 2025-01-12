08e25caa9ddabda357fa97acc6356c1c5f003762
Lurk <bargamon@gmail.com>
2025-01-12 12:58:21 +0100

    rename benches so it makes more sense

--- benches/throughput.rs
+++ benches/throughput.rs
@@ -9,10 +9,10 @@ use yamd::deserialize;
 const LONG_VALID_YAMD: &str = include_str!("./human_input.yamd");
 /// random tokens with long lines
 /// output of yamd_utils random -m=100 352343
-const RANDOM_LONG_LINES: &str = include_str!("./random_token_long_lines.yamd");
+const RANDOM_LOW_DENSITY: &str = include_str!("./random_token_low_density.yamd");
 /// random tokens with short lines
 /// output of yamd_utils random -m=10 352343
-const RANDOM_SHORT_LINES: &str = include_str!("./random_token_short_lines.yamd");
+const RANDOM_HIGH_DENSITY: &str = include_str!("./random_token_high_density.yamd");
 
 fn long_valid(c: &mut Criterion) {
     let mut group = c.benchmark_group("throughput");
@@ -25,18 +25,18 @@ fn long_valid(c: &mut Criterion) {
 
 fn random_long_lines(c: &mut Criterion) {
     let mut group = c.benchmark_group("throughput");
-    group.throughput(Throughput::Bytes(RANDOM_LONG_LINES.len() as u64));
-    group.bench_function("~346kb of random tokens with long lines", |b| {
-        b.iter(|| deserialize(black_box(RANDOM_LONG_LINES)))
+    group.throughput(Throughput::Bytes(RANDOM_LOW_DENSITY.len() as u64));
+    group.bench_function("~346kb with low density of tokens", |b| {
+        b.iter(|| deserialize(black_box(RANDOM_LOW_DENSITY)))
     });
     group.finish();
 }
 
 fn random_short_lines(c: &mut Criterion) {
     let mut group = c.benchmark_group("throughput");
-    group.throughput(Throughput::Bytes(RANDOM_SHORT_LINES.len() as u64));
-    group.bench_function("~344kb of random tokens with short lines", |b| {
-        b.iter(|| deserialize(black_box(RANDOM_SHORT_LINES)))
+    group.throughput(Throughput::Bytes(RANDOM_HIGH_DENSITY.len() as u64));
+    group.bench_function("~344kb with high density of tokens", |b| {
+        b.iter(|| deserialize(black_box(RANDOM_HIGH_DENSITY)))
     });
     group.finish();
 }
